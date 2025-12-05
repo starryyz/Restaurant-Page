c793d80f0f2258546c72bc78bcfdd26d4569d3ad
abethzimmer06@gmail.com <abethzimmer10@gmail.com>
2025-12-05 12:00:49 -0500

    home page

--- home.js
+++ home.js
@@ -9,6 +9,13 @@ window.addEventListener('scroll', () => {
   }
 });
 
-if (window.scrollY > scrollTrigger) {
-  attachedHeader.style.display = 'block';
-}
+
+const cloud = document.querySelector(".text-wrapper");
+let hasAnimated = false;
+
+window.addEventListener("scroll", () => {
+  if (!hasAnimated && window.scrollY > 300) {
+    cloud.classList.add("animate__animated", "animate__bounceIn");
+    hasAnimated = true;
+  }
+});
